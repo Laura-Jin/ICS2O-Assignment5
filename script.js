c1d86a1e6568732180d5d7945b6fdb25936b4f8f
LauraJin8 <Laura.jin@ocsbstudent.ca>
2021-02-22 18:54:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('area').addEventListener('click', areaButton)
+document.getElementById('circumference').addEventListener('click', circumferenceButton)
+
+let diameter = 0
+let area = 0
+let pi = Math.PI
+
+function areaButton() {
+  diameter = document.getElementById('number').value
+  diameter = parseFloat(diameter)
+  pi = parseFloat(pi)
+
+  area = pi * (diameter / 2) ** 2
+  document.getElementById('answer-area').innerHTML = area
+}
+
+function circumferenceButton () {
+  diameter = document.getElementById('number').value
+  diameter = parseFloat(diameter)
+  pi = parseFloat(pi)
+
+  circumference = pi * diameter
+  document.getElementById('answer-circumference').innerHTML = circumference
+}
